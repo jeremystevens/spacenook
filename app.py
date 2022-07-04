45daa50c1c46ebbe03973d35c07b001f729bd9ef
jeremystevens <jeremiahstevens@gmail.com>
2022-07-04 09:48:37 -0700

    Added User Login

--- app.py
+++ app.py
@@ -20,7 +20,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 __version__ = '0.0.1'  # current version
-
+__revision__ = 0
 # ============================================================
 """ app.py: - the main flask server """
 # ============================================================
--- views/main_page/index_views.py
+++ views/main_page/index_views.py
@@ -21,13 +21,16 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 __version__ = '0.0.1'  # current version
 __author__ = 'Jeremy Stevens'  # author
-__revision__ = '1' # revision
+__revision__ = '1'  # revision
+
 # ============================================================
+import time
 
 from sqlalchemy import create_engine
 import flask
 import flask_login
 from werkzeug.security import check_password_hash, generate_password_hash
+
 """ index_views.py - main index page """
 # ============================================================
 
@@ -42,28 +45,39 @@ engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 
 bp = Blueprint("index_views", __name__, url_prefix="/")
 
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["email"]
+        password = request.form["password"]
+        user = Users.query.filter_by(email=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                session["user_id"] = user.id
+                print(session["user_id"])
+                return redirect(url_for("index_views.index"))
+            else:
+                flash("Incorrect username or password")
+        else:
+            flash("User not found")
+    return render_template("main_page/index.html")
+
+
 #  index and login page
 @bp.route("/", methods=["GET", "POST"])
 def index():
     # if user is already logged in
     if "user_id" in session:
-        pass
-        #return redirect(url_for("main_page.main_page"))
-    # if login form is submitted
-    if flask.request.method == 'POST':
-        username = flask.request.form['username']
-        password = flask.request.form['password']
-    # if GET request show login form
+        return redirect(url_for("main_page.main_page"))
     return render_template("main_page/index.html")
 
 
 # register new account
 @bp.route("/register", methods=["GET", "POST"])
 def register():
-
     if request.method == 'GET':
-
-        return render_template("main_page/register.html")
+        return render_template("main_page/index.html")
         # get form data
     if request.method == 'POST':
         first_name = request.form['first_name']
@@ -72,7 +86,7 @@ def register():
         email = request.form['email']
         # check if email is already in use
         if Users.query.filter_by(email=email).first():
-            flash ("Email already in use, please use another email")
+            flash("Email already in use, please use another email")
             return redirect(url_for("index_views.register"))
         # create new user in Users
         users = Users()
@@ -81,6 +95,3 @@ def register():
         flash("Account created, you can now login")
         # redirect to login page after registration
         return redirect(url_for("index_views.index"))
-
-
-
